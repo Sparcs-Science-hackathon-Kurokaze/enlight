85c5392632da13168482a29c0cb128fd7cd2a31b
raspberry-pi <raspberry-pi@kakao.com>
2024-10-19 20:07:56 +0900

    Update scenarios

--- constants/category.ts
+++ constants/category.ts
@@ -1,10 +1,23 @@
 export const categories = {
-  physics: "물리",
-  chemistry: "화학",
-  biology: "생명과학",
-  astronomy: "천문학",
-  mathematics: "수학",
-  computer: "컴퓨터",
-  neuroscience: "뇌과학",
-  preparing: "준비중",
+    physics: "물리",
+    chemistry: "화학",
+    biology: "생명과학",
+    astronomy: "천문학",
+    mathematics: "수학",
+    computer: "컴퓨터",
+    neuroscience: "뇌과학",
+    preparing: "준비중",
 };
+
+export const scenarios = {
+    biology: [
+        {
+            id: 0,
+            title: "중생대 동물과 공룡"
+        }, 
+        {
+            id: 1,
+            title: "바이러스와 박테리아"
+        }
+    ]
+} 
