a7495dcae6faf7ae7cabaa87f7757072cea18185
jjangdol6954_DGIST <106512138+jjangdol-DGIST@users.noreply.github.com>
2024-10-19 15:16:45 +0900

    apply button

--- components/atom/Button.tsx
+++ components/atom/Button.tsx
@@ -7,7 +7,7 @@ export default function Button({ text, onClick }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition-colors duration-200 whitespace-nowrap overflow-hidden text-ellipsis max-w-xs"
+      className="px-4 py-2 bg-normal text-text rounded hover:bg-selection transition-colors duration-200 whitespace-nowrap overflow-hidden text-ellipsis max-w-xs"
     >
       {text}
     </button>
